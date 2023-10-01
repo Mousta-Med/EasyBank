015c3e6982ef7c420bb297e91a6105bb5254f1a5
Mohammed Moustarhfir <112891663+Mousta-Med@users.noreply.github.com>
2023-10-01 21:58:17 +0100

    EAS-41 <ajouter operation fun>

--- src/com/EasyBank/daoImpl/OperationDaoImpl.java
+++ src/com/EasyBank/daoImpl/OperationDaoImpl.java
@@ -1,6 +1,45 @@
 package com.EasyBank.daoImpl;
 
 import com.EasyBank.dao.OperationDao;
+import com.EasyBank.entity.Operation;
+import com.EasyBank.util.DbConnection;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Types;
+import java.util.Optional;
 
 public class OperationDaoImpl implements OperationDao {
+    Connection connection = DbConnection.createConnection();
+    @Override
+    public Optional<Operation> ajouterOperation(Operation operation) {
+        String query = "INSERT INTO Operation(numero, dateOperation, montant, type, compteCourant, employe) VALUES (?,?,?,?,?,?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, operation.getNumero());
+            preparedStatement.setDate(2, Date.valueOf(operation.getDateCreation()));
+            preparedStatement.setDouble(3, operation.getMontant());
+            preparedStatement.setObject(4, operation.getType(), Types.OTHER);
+            preparedStatement.setString(5, operation.getCompte().getNemuro());
+            preparedStatement.setString(6, operation.getEmployé().getMatricul());
+            Integer result = preparedStatement.executeUpdate();
+            if (result != 0) {
+                return Optional.of(operation);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Integer supprimerOperation(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Optional<Operation> chercherOperation(Integer integer) {
+        return null;
+    }
 }
